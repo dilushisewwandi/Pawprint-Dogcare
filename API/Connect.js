ff49e91bc31f7c81a36a324a8fad08fbad2cdfee
Dilushika <dilushisewwandi89@gmail.com>
2024-10-31 17:45:53 +0530

    Use environment variables for database credentials

--- API/Connect.js
+++ API/Connect.js
@@ -1,23 +1,3 @@
-// import mysql from "mysql";
-
-// // Create and export the connection
-// export const db = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "", 
-//     database: "pawprints",
-//     port: 3306
-// });
-
-// // Connect to the database
-// db.connect((err) => {
-//     if (err) {
-//         console.error("Database connection failed: ", err);
-//         return;
-//     }
-//     console.log("Connected to database.");
-// });
-
 import mysql from "mysql";
 import dotenv from "dotenv";
 
