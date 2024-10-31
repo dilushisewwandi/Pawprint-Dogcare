e30b456cfa53b0eee4873bbe3c21e61ecdd00915
Dilushika <dilushisewwandi89@gmail.com>
2024-10-31 17:00:27 +0530

    Add API controller files and update AdopterManage.js

--- API/routes/AdopterManage.js
+++ API/routes/AdopterManage.js
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post("/add", addAdopter);
 router.delete("/delete/:id", deleteAdopter);
 router.put("/update/:id", updateAdopter);
-router.get('/find/:searchBy/:searchValue', findAdopters);
+router.get('/find/:searchBy/:searchValue?', findAdopters); 
 
 export default router;
